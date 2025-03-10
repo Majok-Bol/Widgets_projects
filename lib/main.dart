948b0f364992ac32e7f73dea791d25a377e5ede4
Majok-Bol <bolmajok001@gmail.com>
2025-03-10 12:26:57 +0300

    added margin and padding

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,17 @@ class MyApp extends StatelessWidget {
           ),
           centerTitle: true,
         ),
+
         body: Center(
-          child: Text(
-            'Flutter is Google’s UI toolkit for crafting beautiful, natively compiled iOS and Android apps from a single code base. To build any application we start with widgets – The building block of Flutter applications. Widgets describe what their view should look like given their current configuration and state. It includes a text widget, row widget, column widget, container widget, and many more',
-            style: TextStyle(fontSize: 30),
-            // textAlign: TextAlign.center,
+          child: Container(
+            margin: EdgeInsets.all(5),
+            padding: EdgeInsets.all(1),
+            decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+            child: Text(
+              'Flutter is Google’s UI toolkit for crafting beautiful, natively compiled iOS and Android apps from a single code base. To build any application we start with widgets – The building block of Flutter applications. Widgets describe what their view should look like given their current configuration and state. It includes a text widget, row widget, column widget, container widget, and many more',
+              style: TextStyle(fontSize: 30),
+              // textAlign: TextAlign.center,
+            ),
           ),
         ),
       ),
