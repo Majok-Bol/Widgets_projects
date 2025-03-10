cb44b30e23f00e4045944466c5f525ee11e06ae8
Majok-Bol <bolmajok001@gmail.com>
2025-03-10 11:01:22 +0300

    added background to the title

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        //add top bar
         appBar: AppBar(
+          //add title text
           title: Text(
-            'My first flutter application',
+            'My first app',
             style: TextStyle(fontSize: 30, backgroundColor: Colors.blueGrey),
           ),
         ),
+        //body of the page
         body: Center(
+          //add text to child body
           child: Text(
             'My first hello world in flutter',
             style: TextStyle(fontSize: 30, backgroundColor: Colors.teal),
           ),
+        ),
+        //add a button
+        floatingActionButton: FloatingActionButton(
+          onPressed: null,
+          child: Text('Click here'),
         ),
       ),
     );
