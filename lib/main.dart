fe3061b84f2ba572762c239d273a54e3c1c62182
Majok-Bol <bolmajok001@gmail.com>
2025-03-10 11:07:55 +0300

    centered title

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        //add top bar
-        appBar: AppBar(
-          //add title text
-          title: Text(
-            'My first app',
-            style: TextStyle(fontSize: 30, backgroundColor: Colors.blueGrey),
-          ),
-        ),
-        //body of the page
-        body: Center(
-          //add text to child body
-          child: Text(
-            'My first hello world in flutter',
-            style: TextStyle(fontSize: 30, backgroundColor: Colors.teal),
-          ),
-        ),
-        //add a button
-        floatingActionButton: FloatingActionButton(
-          onPressed: null,
-          child: Text('Click here'),
-        ),
+        appBar: AppBar(title: Text('My first app'), centerTitle: true),
       ),
     );
   }
